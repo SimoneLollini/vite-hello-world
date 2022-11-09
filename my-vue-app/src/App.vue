5b05b5a43d7dfc3ecc839dda337929bd84eed100
SimoneLollini <lollinisimone@gmail.com>
2022-11-09 16:43:40 +0100

    Add jumbotron

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,10 +1,13 @@
 <script>
 // #1 importo qui eventuali componenti
 import AppTitle from "./components/AppTitle.vue";
+import AppHero from "./components/AppHero.vue";
+
 export default {
   // #2 definisco una proprietá components per registrare i componenti importati al punto 1
   components: {
     AppTitle,
+    AppHero,
   }
 
 }
@@ -16,6 +19,7 @@ export default {
 
 <template>
   <AppTitle />
+  <AppHero />
 </template>
 
 
---
+++ my-vue-app/src/components/AppHero.vue
@@ -0,0 +1,18 @@
+<script>
+export default {
+    name: "AppHero",
+}
+</script>
+
+<template>
+    <div class="hero">JUMBOTRON</div>
+</template>
+
+<style>
+.hero {
+    background-color: blueviolet;
+    padding: 2rem;
+    font-weight: 600;
+    font-size: 2rem;
+}
+</style>

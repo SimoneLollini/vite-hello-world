767ad9dd23db34d73c70cfd1b422c7ddda081893
SimoneLollini <lollinisimone@gmail.com>
2022-11-09 15:46:09 +0100

    Add title

--- my-vue-app/src/App.vue
+++ my-vue-app/src/App.vue
@@ -1,10 +1,10 @@
 <script>
 // #1 importo qui eventuali componenti
-import { AppTitle } from "./components/AppTitle.vue";
+import AppTitle from "./components/AppTitle.vue";
 export default {
   // #2 definisco una proprietá components per registrare i componenti importati al punto 1
   components: {
-    AppTitle
+    AppTitle,
   }
 
 }
--- my-vue-app/src/components/AppTitle.vue
+++ my-vue-app/src/components/AppTitle.vue
@@ -1,11 +1,11 @@
 <script>
 export default {
-
+  name: "AppTitle",
 }
 </script>
 
 <template>
-  <h1></h1>
+  <h1>La mia prima app con Vite!</h1>
 
 
 </template>
